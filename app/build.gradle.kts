af5d0d4daed87584dedf49ee21c7d3ca3d5fded4
yugo-miyabe <yuni42yuyuyu@gmail.com>
2025-05-05 23:53:33 +0900

    Hiltの依存関係を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -77,4 +79,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 }
--- app/src/main/java/jp/yuyuyu/habits/HabitsApplication.kt
+++ app/src/main/java/jp/yuyuyu/habits/HabitsApplication.kt
@@ -1,5 +1,7 @@
 package jp.yuyuyu.habits
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class HabitsApplication : Application()
